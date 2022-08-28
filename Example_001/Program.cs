7900d9534cdafaafc30c46270d071f448d454f8c
Maksim S. Monastyrshin <bubzuk_ka@mail.ru>
2022-08-28 23:12:47 +1000

    Example_002_Hi ver 1

--- Example_001/Program.cs
+++ Example_001/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Console.WriteLine("Hello, World!");
 Console.WriteLine("2 hello world");
-
+Console.WriteLine("2 hello world");
 
 
 
---
+++ Example_002_Hi/Program.cs
@@ -0,0 +1,8 @@
+﻿//Запрос имени пользователя и вывод приветствия
+Console.Write("Ввведите имя пользователя: ");
+string Username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(Username);
+
+
+

b4a73facf6ca9a481c38fbd809376d870eedd1f9
Maksim S. Monastyrshin <bubzuk_ka@mail.ru>
2022-08-29 21:30:14 +1000

    Task_friends_n_dog release ver 1.0

--- Task_friends_n_dog/Program.cs
+++ Task_friends_n_dog/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int count = 0,
+    friend = 2;
+
+double distance = 10000,
+       FirstFriendSpeed = 1,
+       SecondFriendSpeed = 2,
+       DogSpeed = 3;
+Time = 0;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        Time = distance / (FirstFriendSpeed + DogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        Time = distance / (SecondFriendSpeed + DogSpeed);
+        friend = 1;
+    }
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * Time;
+    count++;
+}
+    Console.Write("Собака пробежит: ");
+    Console.Write(count);
+    Console.Write(" раз.");

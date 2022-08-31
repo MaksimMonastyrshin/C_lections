f393c16944baa4df2d33fb4ab5ba329474dd78ad
Maksim S. Monastyrshin <bubzuk_ka@mail.ru>
2022-08-31 22:32:05 +1000

    add final Ex 009 task 4

---
+++ Example_009_Smnr_1_task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Write("Введите первое число, а = ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число, b = ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число, b = ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > a) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("{0}, {1}, {2} -> {3}", a, b, c, max);
+
+
+
+

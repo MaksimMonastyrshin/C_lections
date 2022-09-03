363639b44afa6fb93b0d0dad4cc9ac7d77904592
Maksim S. Monastyrshin <bubzuk_ka@mail.ru>
2022-09-03 19:54:31 +1000

    adding tasks 10, 13, 15

---
+++ Example_010_test/Program.cs
@@ -0,0 +1,6 @@
+﻿// приколы от майков
+double z = 10 / 4; //результат равен 2
+Console.WriteLine(z);
+
+z = 10 / 4.0; //результат равен 2,5
+Console.WriteLine(z);
---
+++ Example_011_Rnd_XX_max/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = new Random().Next(10, 100);
+int a1 = num / 10;
+int a2 = num % 10;
+int max = a1;
+if (max < a2)
+    {
+        max = a2;
+    }
+Console.WriteLine($"начальное число {num}");
+Console.WriteLine($"максимольное число {max}");

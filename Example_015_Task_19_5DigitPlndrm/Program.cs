32f0653f814bcf072beec016fb274b5dda565642
MaksimMonastyrshin <Bubzuk_Ka@mail.ru>
2022-09-07 14:58:13 +1000

    Task 19 added

---
+++ Example_015_Task_19_5DigitPlndrm/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет
+// является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.WriteLine("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+int LeftDigit1 = num / 10000;
+int LeftDigit2 = (num % 10000) / 1000;
+int RightDigit1 = num % 10;
+int RightDigit2 = (num % 100) / 10;
+if(LeftDigit1 == RightDigit1 && LeftDigit2 == RightDigit2)
+{
+    Console.WriteLine($"{num} -> да");
+}
+else
+{
+    Console.WriteLine($"{num} -> нет");
+}

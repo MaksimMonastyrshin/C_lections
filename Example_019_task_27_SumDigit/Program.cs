2bcdc1c2e61bd2b47992ed2484f6d2d0a978a231
Maksim S. Monastyrshin <bubzuk_ka@mail.ru>
2022-09-11 19:44:43 +1000

    Added task 27

---
+++ Example_019_task_27_SumDigit/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{num} -> {SumDigCount(num)}");
+Console.WriteLine();
+
+int SumDigCount(int num) // Метод нахождения суммы чисел всех разрядов
+{
+    int SumDig = 0;
+do  
+    {
+        SumDig = SumDig + num % 10;
+        num = num / 10;
+    }
+while (num > 0);
+return SumDig;
+}

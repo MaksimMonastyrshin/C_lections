707e9dc8d4aaf8fa7755a11512ddb3e4796edfa5
Maksim S. Monastyrshin <bubzuk_ka@mail.ru>
2022-10-02 14:31:43 +1000

    Added Task 66

---
+++ Example_036_Task_66_SummIn/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Clear();
+Console.Write("Введите начальное число M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное число N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m>n)
+{
+    Console.WriteLine("Введены ошибочные значения: M > N.");
+}
+else
+{
+    Console.WriteLine($"M = {m}, N = {n} -> {SumNatural(m, n)}");
+}
+
+// Метод рекурсивного подсчета суммы натуральных чисел в диапазоне: [paramStart, paramEnd].
+int SumNatural(int paramStart, int paramEnd)
+{
+    int sumNat = paramStart;
+    if (paramStart == paramEnd)
+    {
+        return sumNat;
+    }
+    return sumNat + SumNatural(paramStart + 1, paramEnd);
+}
